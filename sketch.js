a93245c44dcc591736b946ea67cbcda382690d99
ks7150 <144281932+ks7150@users.noreply.github.com>
2023-09-13 15:29:25 -0400

    week2

--- sketch.js
+++ sketch.js
@@ -1,5 +1,18 @@
 function setup() {
+    createCanvas (600,600)
+    background (200,234,240)
 }
 
 function draw() {
+fill(200,123,120)
+stroke ("black")
+    ellipse(120, 130, 67, [67])
+    fill(200,123,150)
+    stroke ("white")
+    ellipse(230, 600, 200, [200])
+    stroke ("grey")
+    fill("white") 
+    strokeWeight(10)
+    rect(450, 100, 100, 100)
+
 }
