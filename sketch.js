fc63562465b3ccc85a9ac82f13d313e2913adf0d
ks7150 <144281932+ks7150@users.noreply.github.com>
2023-09-13 16:26:18 -0400

    Revert "Update sketch.js"
    This reverts commit cbc52e022e82e4d9fdb7e3f4ed7b6d280ca55b56.

--- sketch.js
+++ sketch.js
@@ -1,21 +1,18 @@
-let eyeWidth = 200;
-
-
-
 function setup() {
     createCanvas (600,600)
     background (200,234,240)
 }
 
 function draw() {
-    fill("white")
-    strokeWeight(5)
-    arc(250, 250, 80, 80, 0, PI+QUARTER_PI, [PIE])
-    arc(200, 200, 80, 80, 0, PI+QUARTER_PI, [PIE])
-    noFill();
-    arc(200, 255, 60, 60, HALF_PI, PI);
-    fill("black")
-    ellipse (250,250,20,20)
-ellipse(200,200,20,20)
+fill(200,123,120)
+stroke ("black")
+    ellipse(120, 130, 67, [67])
+    fill(200,123,150)
+    stroke ("white")
+    ellipse(230, 600, 200, [200])
+    stroke ("grey")
+    fill("white") 
+    strokeWeight(10)
+    rect(450, 100, 100, 100)
 
 }
